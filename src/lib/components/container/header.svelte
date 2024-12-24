b364cc7b0ce565b93ff34b8a865d6e402108849f
Jason Maliquid <jasonm2822@gmail.com>
2024-12-24 20:10:00 +0800

    ✅ Added headerNav

--- src/lib/components/container/header.svelte
+++ src/lib/components/container/header.svelte
@@ -6,6 +6,6 @@
 <header
 	class="flex w-dvw items-center justify-between border-b px-3 py-2 shadow-xl dark:shadow-white/10"
 >
-	<img src={Logo} alt="Logo" class="size-11" />
+	<img src={Logo} alt="Logo" class="size-8" />
 	<HeaderNav />
 </header>
