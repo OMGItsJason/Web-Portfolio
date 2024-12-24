5d337ee0dccae62c8e7e56eb358767751446f0ce
Jason Maliquid <jasonm2822@gmail.com>
2024-12-24 20:30:31 +0800

    ✅ Add maintenance page with logo and message

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Logo from '@/assets/logo/Logo.png';
+</script>
+
+<div
+	class="absolute bottom-0 top-0 z-50 flex h-svh w-svw items-center justify-center bg-white dark:bg-black"
+>
+	<div class="flex flex-col items-center text-center">
+		<img src={Logo} alt="Logo" class="size-40" />
+		<h1 class="text-xl font-bold text-white dark:text-white md:text-3xl">
+			👨🏽‍🔧 Page Under Maintenance 👨🏽‍🔧
+		</h1>
+		<p class="text-lg font-normal text-black dark:text-white md:text-xl">
+			We apologize for the inconvenience. This page is currently undergoing maintenance to improve
+			your experience. Please check back later. Thank you for your patience!
+		</p>
+		<span class="text-xl md:text-4xl">🚧🚧🚧</span>
+	</div>
+</div>
