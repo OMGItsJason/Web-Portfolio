241ced1b8488deb9ffcc4a5617b916c9447e7b41
Jason Maliquid <jasonm2822@gmail.com>
2024-12-24 22:08:36 +0800

    ✅ Integrate enhanced-img for logo optimization and update header component

--- src/lib/components/container/header.svelte
+++ src/lib/components/container/header.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import HeaderNav from '@/components/layout/headerNav.svelte';
 	import SearchBar from '@/components/layout/searchBar.svelte';
-	import Logo from '@/assets/logo/Logo.png';
+	import Logo from '@/assets/logo/Logo.png?enhanced';
 </script>
 
 <header
 	class="flex w-dvw items-center justify-between border-b px-3 py-2 shadow-xl dark:shadow-white/10"
 >
-	<img src={Logo} alt="Logo" class="size-8" />
+	<a href="/" aria-label="Home">
+		<enhanced:img src={Logo} alt="Logo" class="size-8" />
+	</a>
 	<SearchBar />
 	<HeaderNav />
 </header>
