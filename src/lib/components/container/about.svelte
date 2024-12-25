3d3f963665ee52bff3623170a14166c8aced5ffc
Jason Maliquid <jasonm2822@gmail.com>
2024-12-25 20:14:35 +0800

    🚧 About component

---
+++ src/lib/components/container/about.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import * as Card from '@/components/ui/card';
+	import { MapPin, School, GraduationCap } from 'lucide-svelte';
+
+	const info = [
+		{
+			icon: School,
+			title: 'Freshmen - Senior',
+			description: 'Tabaco National High School'
+		},
+		{
+			icon: GraduationCap,
+			title: 'BS in Information Technology',
+			description: 'STI College Legazpi'
+		},
+		{
+			icon: MapPin,
+			title: 'Location',
+			description: 'Tabaco City, Albay, Philippines'
+		}
+	];
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>About</Card.Title>
+		<Card.Description>
+			<p class="text-sm">
+				Seeking opportunities to apply my skills in a dynamic environment and contribute to
+				impactful projects.
+			</p>
+		</Card.Description>
+	</Card.Header>
+	<Card.Content class="space-y-3">
+		{#each info as { icon, title, description }}
+			<div class="flex items-center gap-3">
+				<svelte:component this={icon} class="size-4" />
+				<div class="flex flex-col">
+					<p class="text-base font-semibold">{description}</p>
+					<h1 class="text-xs font-light">{title}</h1>
+				</div>
+			</div>
+		{/each}
+	</Card.Content>
+</Card.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,17 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '@/components/container/header.svelte';
 	import BodyHead from '@/components/container/bodyHead.svelte';
+	import About from '@/components/container/about.svelte';
 </script>
 
 <ModeWatcher />
 <div class="w-svw">
 	<Header />
-	<div class="p-2 md:px-10 2xl:px-20">
+	<div class="space-y-3 p-2 md:px-10 2xl:px-20">
 		<BodyHead />
+		<div class="grid">
+			<About />
+			{@render children()}
+		</div>
 	</div>
 </div>
-{@render children()}
