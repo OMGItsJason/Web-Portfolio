f523d0513aff20c4b57b44ae2176a266bcf3a122
Jason Maliquid <jasonm2822@gmail.com>
2024-12-25 10:45:41 +0800

    💄 Add Avatar and Carousel components with corresponding styles and functionality

---
+++ src/lib/components/container/bodyHead.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as Card from '@/components/ui/card';
+	import TechCarousel from '@/components/layout/techCarousel.svelte';
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<TechCarousel />
+	</Card.Header>
+</Card.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,14 @@
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '@/components/container/header.svelte';
+	import BodyHead from '@/components/container/bodyHead.svelte';
 </script>
 
 <ModeWatcher />
 <div class="w-svw">
 	<Header />
-	<div></div>
+	<div class="p-2">
+		<BodyHead />
+	</div>
 </div>
 {@render children()}
