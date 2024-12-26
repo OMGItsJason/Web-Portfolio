c5b109f0739bdd899deb0da8dc4efb29e677ad0e
Jason Maliquid <jasonm2822@gmail.com>
2024-12-26 17:59:32 +0800

    💄 Add BodyNav component for improved navigation structure

---
+++ src/lib/components/container/bodyNav.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Card from '@/components/ui/card';
+
+	const navItem = [
+		{
+			title: 'Tech-Stack',
+			href: '/'
+		},
+		{
+			title: 'Projects',
+			href: '/projects'
+		},
+		{
+			title: 'Certificates',
+			href: '/certificates'
+		}
+	];
+</script>
+
+<Card.Root>
+	<Card.Header class="flex items-center p-2 text-center">
+		<Card.Title class="flex gap-5 p-0">
+			{#each navItem as item}
+				<a href={item.href} class="text-base">{item.title}</a>
+			{/each}
+		</Card.Title>
+	</Card.Header>
+</Card.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '@/components/container/header.svelte';
 	import BodyHead from '@/components/container/bodyHead.svelte';
+	import BodyNav from '@/components/container/bodyNav.svelte';
 	import About from '@/components/container/about.svelte';
 </script>
 
@@ -12,6 +13,7 @@
 	<Header />
 	<div class="space-y-3 p-2 md:px-10 2xl:px-20">
 		<BodyHead />
+		<BodyNav />
 		<div class="grid">
 			<About />
 			{@render children()}
