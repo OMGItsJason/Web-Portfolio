307588f9c1d7b77fe5e9a97ff5bab0471514b653
Jason Maliquid <jasonm2822@gmail.com>
2024-12-30 10:16:10 +0800

    💄 Add Separator component and update bodyNav layout for improved navigation styling

--- src/lib/components/container/bodyNav.svelte
+++ src/lib/components/container/bodyNav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as Card from '@/components/ui/card';
 	import { Button } from '@/components/ui/button';
+	import { Separator } from '@/components/ui/separator';
 
 	const navItem = [
 		{
@@ -22,7 +23,8 @@
 	<Card.Header class="flex items-center p-2 text-center md:items-start md:text-left">
 		<Card.Title class="flex p-0">
 			{#each navItem as item}
-				<Button href={item.href} variant="link">{item.title}</Button>
+				<Button href={item.href} variant="link" class="md:text-lg">{item.title}</Button>
+				<Separator orientation="vertical" />
 			{/each}
 		</Card.Title>
 	</Card.Header>
