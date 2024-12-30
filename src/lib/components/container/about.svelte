06271d5d8243ebf7058729f5ade3cce727b9f779
Jason Maliquid <jasonm2822@gmail.com>
2024-12-30 10:13:10 +0800

    💄 Enhance About section styling for improved readability and responsiveness

--- src/lib/components/container/about.svelte
+++ src/lib/components/container/about.svelte
@@ -23,21 +23,25 @@
 
 <Card.Root>
 	<Card.Header>
-		<Card.Title>About</Card.Title>
+		<Card.Title class="text-xl font-bold md:text-3xl">ABOUT</Card.Title>
 		<Card.Description>
-			<p class="text-sm">
+			<p class="text-sm md:hidden">
 				Seeking opportunities to apply my skills in a dynamic environment and contribute to
 				impactful projects.
+			</p>
+			<p class="hidden text-black dark:text-white md:block md:text-lg">
+				I'm searching for opportunities where i can use my abilities in an interesting work
+				environment and contribute to significant projects.
 			</p>
 		</Card.Description>
 	</Card.Header>
 	<Card.Content class="space-y-3">
 		{#each info as { icon, title, description }}
 			<div class="flex items-center gap-3">
-				<svelte:component this={icon} class="size-4" />
+				<svelte:component this={icon} class="size-4 md:size-6" />
 				<div class="flex flex-col">
-					<p class="text-base font-semibold">{description}</p>
-					<h1 class="text-xs font-light">{title}</h1>
+					<p class="text-base font-semibold md:text-xl">{description}</p>
+					<h1 class="text-xs font-light md:text-lg">{title}</h1>
 				</div>
 			</div>
 		{/each}
